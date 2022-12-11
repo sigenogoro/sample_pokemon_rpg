57a2185f96feee35cdd9f727a9dafd1dbc8a96c9
sigenogoro <25719873+sigenogoro@users.noreply.github.com>
2022-12-12 07:25:05 +0900

    クラスを使った ジェネラクスの使い方

--- src/main/kotlin/study/generics/Box2.kt
+++ src/main/kotlin/study/generics/Box2.kt
@@ -1,5 +1,13 @@
 package study.generics
 
-class Sample {
-    
+class Box2<T>(
+    val value: T
+)
+
+fun main() {
+    val boxInt = Box2(value = 1234)
+    val boxString = Box2("アイウエオ")
+
+    println(boxInt.value)
+    println(boxString.value)
 }
